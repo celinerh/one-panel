c4d80733664cf13a3107a01abab6544c895058d7
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-23 22:03:13 +0200

    add react router and mantimeprovider

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,42 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
+    <MantineProvider
+      withGlobalStyles={false}
+      theme={{
+        colors: {
+          primary: [
+            "#f0fdf4",
+            "#dcfce7",
+            "#bbf7d0",
+            "#86efac",
+            "#4ade80",
+            "#22c55e",
+            "#16a34a",
+            "#15803d",
+            "#166534",
+            "#14532d",
+          ],
+        },
+        primaryShade: { light: 4, dark: 8 },
+        colorScheme: "light",
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>
 );
