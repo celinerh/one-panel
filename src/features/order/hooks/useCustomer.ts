03f7d482d3c9b1878504050d895b3336baf8ecfd
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-30 21:56:09 +0200

    add useCustomer hook

---
+++ src/features/order/hooks/useCustomer.ts
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { useToken } from "../../../contexts/TokenContext";
+
+interface Customer {
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  marketing: boolean;
+}
+
+const useCustomer = (customerId: number | null) => {
+  const [customer, setCustomer] = useState<Customer>();
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, setIsPending] = useState<boolean>(true);
+  const { token } = useToken();
+
+  useEffect(() => {
+    if (customerId === null) {
+      return;
+    }
+
+    fetch(`http://localhost:3001/customers/${customerId}`, {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCustomer(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, [customerId]);
+
+  return { customer, error, isPending };
+};
+
+export default useCustomer;
