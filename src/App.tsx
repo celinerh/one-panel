e26ca6ac20534da1f186fac3bf2059362577dd63
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-01 18:16:26 +0200

    fix some styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="md:flex App">
       <Navigation />
-      <h1 className="p-6 mb-8 text-4xl text-center border-b-2 border-gray-200 font-Pacifico md:hidden">
+      <h1 className="p-6 mb-8 text-3xl text-center border-b-2 border-gray-200 sm:text-4xl font-Pacifico md:hidden">
         OnePanel
       </h1>
-      <div className="w-full h-screen p-6 md:p-10">
+      <div className="w-full h-screen p-6 md:p-6 lg:p-10">
         <Outlet />
       </div>
       <BottomNavigation className="md:hidden" />
--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -12,15 +12,15 @@ function BottomNavigation({ className }: { className?: string }) {
     <nav
       className={`fixed bottom-0 left-0 z-[200] p-6 w-full bg-white border-t-2 border-gray-200 ${className}`}
     >
-      <ul className="flex justify-between">
+      <ul className="flex justify-evenly">
         {navLinks.map((navLink, index) => (
           <li key={index}>
             <NavLink
               to={navLink.to}
               className="flex flex-col items-center text-gray-600"
             >
-              <navLink.icon className="text-4xl" />
-              <p>{navLink.label}</p>
+              <navLink.icon className="text-xl sm:text-2xl" />
+              <p className="text-xs sm:text-sm">{navLink.label}</p>
             </NavLink>
           </li>
         ))}
