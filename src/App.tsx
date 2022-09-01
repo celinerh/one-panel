0b5b07475f419874fad25dd0a18ff7d48878c297
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-01 15:57:47 +0200

    show bottom navigation on smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import BottomNavigation from "./components/BottomNavigation";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="flex App">
+    <div className="md:flex App">
       <Navigation />
-      <div className="w-full h-screen p-10">
+      <h1 className="p-6 mb-8 text-4xl text-center border-b-2 border-gray-200 font-Pacifico md:hidden">
+        OnePanel
+      </h1>
+      <div className="w-full h-screen p-6 md:p-10">
         <Outlet />
       </div>
+      <BottomNavigation className="md:hidden" />
     </div>
   );
 }
