10b9c259d1db7fdd58b499a490f6eef90339e123
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-01 18:15:33 +0200

    hide columns on smaller screens

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -42,6 +42,8 @@ function Orders() {
     <tr key={order.id}>
       <td>
         <Badge
+          variant="outline"
+          size="sm"
           color={
             order.status === "shipped"
               ? "green"
@@ -54,16 +56,23 @@ function Orders() {
         </Badge>
       </td>
       <td>#{order.id}</td>
-      <td>{order.date.toString()}</td>
-      <td>#{order.customerId}</td>
-      <td>
+      <td className="hidden sm:table-cell whitespace-nowrap">
+        {order.date.toString()}
+      </td>
+      <td className="hidden lg:table-cell">#{order.customerId}</td>
+      <td className="hidden md:table-cell">
         {order.products.reduce((acc, curr) => {
           return (acc += curr.quantity);
         }, 0)}
       </td>
 
       <td>
-        <Button component={Link} to={`/order/${order.id}`} color="tertiary">
+        <Button
+          component={Link}
+          to={`/order/${order.id}`}
+          color="tertiary"
+          size="xs"
+        >
           Edit
         </Button>
       </td>
@@ -75,16 +84,18 @@ function Orders() {
       <Heading title="Orders" />
       <Table
         verticalSpacing="xs"
-        horizontalSpacing="xl"
+        horizontalSpacing="lg"
         className="bg-white border-2 border-gray-200"
       >
         <thead>
           <tr className="bg-gray-50">
-            <th className="w-48">Status</th>
-            <th>Order ID</th>
-            <th>Date</th>
-            <th>Customer ID</th>
-            <th>Quantity</th>
+            <th className="md:w-32 lg:w-40">Status</th>
+            <th className="whitespace-nowrap">Order ID</th>
+            <th className="hidden sm:table-cell">Date</th>
+            <th className="hidden whitespace-nowrap lg:table-cell">
+              Customer ID
+            </th>
+            <th className="hidden md:table-cell">Quantity</th>
             <th>Action</th>
           </tr>
         </thead>
