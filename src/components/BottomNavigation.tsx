447746e0626ee4039946c4fd42bb24bf2625dd46
cellle <91531988+celinerh@users.noreply.github.com>
2022-09-01 15:54:58 +0200

    add bottom navigation

---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,32 @@
+import { HiClipboardList, HiShoppingBag, HiUsers } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  { to: "/products", label: "Products", icon: HiShoppingBag },
+  { to: "/orders", label: "Orders", icon: HiClipboardList },
+  { to: "/customers", label: "Customers", icon: HiUsers },
+];
+
+function BottomNavigation({ className }: { className?: string }) {
+  return (
+    <nav
+      className={`fixed bottom-0 left-0 z-[200] p-6 w-full bg-white border-t-2 border-gray-200 ${className}`}
+    >
+      <ul className="flex justify-between">
+        {navLinks.map((navLink, index) => (
+          <li key={index}>
+            <NavLink
+              to={navLink.to}
+              className="flex flex-col items-center text-gray-600"
+            >
+              <navLink.icon className="text-4xl" />
+              <p>{navLink.label}</p>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default BottomNavigation;
